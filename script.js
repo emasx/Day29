f5f5eb0f608396c3ae97cad250c0f8134997c14e
emasx <29961897+emasx@users.noreply.github.com>
2022-09-12 23:18:00 +0300

    project #1

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+//1. document.querySelector('.message') selects the element. 
+//2. On this element we can read the textContent property.  .textContent
+
+console.log(document.querySelector('.message').textContent);
